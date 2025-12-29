92ff0fa340c6de5ca2739ec5f612f7b7df1c0090
Samrudhi03-hub <samrujagadale@gmail.com>
2025-12-29 18:24:09 +0530

    Started with Page2

--- src/components/Page2/Page2.jsx
+++ src/components/Page2/Page2.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 const Page2 = () => {
   return (
     <div className='h-screen w-full bg-[#F3E8D6]'>
-        
+        <div className='flex justify-between px-20'>
+            <div>
+                <h3>Featured</h3>
+                <h2>Why Choose Us?</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque illum alias quaerat totam, ut iusto id recusandae a eius iste blanditiis sit cumque. Cumque veniam doloribus, ex tenetur velit et ratione magni? Dicta natus et odio modi quod quibusdam magnam.</p>
+                <button>Know more</button>
+            </div>
+            <img src="./public/coffee-cup1.jpg" alt="" />
+        </div>
+        <div>
+
+        </div>
     </div>
   )
 }
