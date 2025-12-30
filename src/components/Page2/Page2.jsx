b86272331b9b94a776d512cae7e560dae3436925
Samrudhi03-hub <samrujagadale@gmail.com>
2025-12-30 18:06:01 +0530

    Updated page2 content

--- src/components/Page2/Page2.jsx
+++ src/components/Page2/Page2.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 const Page2 = () => {
   return (
-    <div className='h-screen w-full bg-[#F3E8D6]'>
-        <div className='flex justify-between px-20'>
-            <div>
-                <h3>Featured</h3>
-                <h2>Why Choose Us?</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque illum alias quaerat totam, ut iusto id recusandae a eius iste blanditiis sit cumque. Cumque veniam doloribus, ex tenetur velit et ratione magni? Dicta natus et odio modi quod quibusdam magnam.</p>
-                <button>Know more</button>
+    <div className='h-screen w-full bg-[#F3E8D6] px-30'>
+        <div className='flex justify-between'>
+            <div className='bg-white pt-7'>
+                <h3 className='uppercase text-[#3B2F2F] font-bold tracking-wide '>Featured</h3>
+                <h2 className='text-3xl font-extrabold text-[#4d3423] pt-1'>Why Choose Us?</h2>
+                <p className='text-[#674229] text-sm pt-6 leading-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit laboriosam eveniet, magni est in deserunt dignissimos libero. Doloremque non ut molestiae eos aut, architecto, a deserunt consectetur eius, recusandae sunt excepturi ab iste accusantium sint tempora ratione ea? Ex ipsa pariatur voluptates qui! Facilis sapiente unde impedit! Provident, omnis, recusandae aliquam tempora laborum placeat at, culpa nulla dolore minus porro!</p>
+                <button className='bg-[#3e2616] text-white mt-4'>Know more</button>
             </div>
-            <img src="./public/coffee-cup1.jpg" alt="" />
+            <img className='h-100 rounded-b-xl' src="./public/coffee-cup1.jpg" alt="" />
         </div>
+
+
         <div>
 
         </div>
